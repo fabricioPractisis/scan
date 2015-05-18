ca77d4970e3b3e616056754765e6b03eea319276
fabricioPractisis <fabriciocarrion18@outlook.com>
2015-05-18 15:19:06 -0500

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,14 @@
 
 function dale(){
-alert("vamos");
+//alert("vamos");
 cordova.plugins.barcodeScanner.scan(
       function (result) {
-         var s = "Result: " + result.text + "<br/>" +
-	"Format: " + result.format + "<br/>" +
-	"Cancelled: " + result.cancelled;
-	resultDiv.innerHTML = s;
+      	var s = result.text;
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+		$('#recibeScan').html(s);
       }, 
       function (error) {
           alert("Scanning failed: " + error);
