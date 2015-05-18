2247d83568880453a6eb9c53fdf55a1e4ad047a8
fabricioPractisis <fabriciocarrion18@outlook.com>
2015-05-18 18:04:43 -0500

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -4,7 +4,7 @@ cordova.plugins.barcodeScanner.scan(
       function (result) {
 		  var id = result.text;
          alert(id);
-         document.getElementById("recibeScan").value = “+id+”;
+         $("recibeScan").value(id);
       }, 
       function (error) {
           alert("Scanning failed: " + error);
